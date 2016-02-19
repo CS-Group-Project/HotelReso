a5c154feff16298f85ff4c930ef59888f64403c2
samarara <sandes.samararatne@gmail.com>
2016-02-18 20:33:07 -0500

    Fine tuned UI, added a few validation methods

--- HotelReso/Form1.Designer.cs
+++ HotelReso/Form1.Designer.cs
@@ -32,17 +32,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label5 = new System.Windows.Forms.Label();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtTel = new System.Windows.Forms.TextBox();
             this.cmdInsert = new System.Windows.Forms.Button();
             this.cmdUpdate = new System.Windows.Forms.Button();
             this.cmdDelete = new System.Windows.Forms.Button();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dg1 = new System.Windows.Forms.DataGridView();
+            this.txtTableNum = new System.Windows.Forms.TextBox();
+            this.txtTime = new System.Windows.Forms.TextBox();
+            this.txtDate = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtDuration = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,145 +52,168 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(28, 22);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.Size = new System.Drawing.Size(111, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Date";
+            this.label1.Text = "Date (YYYY/MM/DD)";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(28, 80);
+            this.label2.Location = new System.Drawing.Point(28, 76);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Time";
+            this.label2.Text = "Time (1500)";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(28, 134);
+            this.label3.Location = new System.Drawing.Point(28, 130);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(74, 13);
             this.label3.TabIndex = 2;
             this.label3.Text = "Table Number";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(28, 182);
+            this.label4.Location = new System.Drawing.Point(28, 221);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(35, 13);
             this.label4.TabIndex = 3;
             this.label4.Text = "Name";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(28, 38);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 4;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(28, 238);
+            this.label5.Location = new System.Drawing.Point(28, 270);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(22, 13);
+            this.label5.Size = new System.Drawing.Size(107, 13);
             this.label5.TabIndex = 5;
-            this.label5.Text = "Tel";
+            this.label5.Text = "Tel (XXX-XXX-XXXX)";
             // 
-            // comboBox2
+            // txtName
             // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(28, 96);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(108, 21);
-            this.comboBox2.TabIndex = 6;
+            this.txtName.Location = new System.Drawing.Point(28, 237);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(147, 20);
+            this.txtName.TabIndex = 8;
             // 
-            // comboBox3
+            // txtTel
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Location = new System.Drawing.Point(28, 150);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(54, 21);
-            this.comboBox3.TabIndex = 7;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(28, 202);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(147, 20);
-            this.textBox1.TabIndex = 8;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(28, 260);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(149, 20);
-            this.textBox2.TabIndex = 9;
+            this.txtTel.Location = new System.Drawing.Point(28, 292);
+            this.txtTel.Name = "txtTel";
+            this.txtTel.Size = new System.Drawing.Size(149, 20);
+            this.txtTel.TabIndex = 9;
             // 
             // cmdInsert
             // 
-            this.cmdInsert.Location = new System.Drawing.Point(28, 297);
+            this.cmdInsert.Location = new System.Drawing.Point(28, 331);
             this.cmdInsert.Name = "cmdInsert";
             this.cmdInsert.Size = new System.Drawing.Size(149, 30);
             this.cmdInsert.TabIndex = 10;
             this.cmdInsert.Text = "Insert";
             this.cmdInsert.UseVisualStyleBackColor = true;
+            this.cmdInsert.Click += new System.EventHandler(this.cmdInsert_Click);
             // 
             // cmdUpdate
             // 
-            this.cmdUpdate.Location = new System.Drawing.Point(28, 333);
+            this.cmdUpdate.Enabled = false;
+            this.cmdUpdate.Location = new System.Drawing.Point(28, 367);
             this.cmdUpdate.Name = "cmdUpdate";
             this.cmdUpdate.Size = new System.Drawing.Size(149, 30);
             this.cmdUpdate.TabIndex = 11;
             this.cmdUpdate.Text = "Update";
             this.cmdUpdate.UseVisualStyleBackColor = true;
+            this.cmdUpdate.Click += new System.EventHandler(this.cmdUpdate_Click);
             // 
             // cmdDelete
             // 
-            this.cmdDelete.Location = new System.Drawing.Point(28, 371);
+            this.cmdDelete.Enabled = false;
+            this.cmdDelete.Location = new System.Drawing.Point(28, 405);
             this.cmdDelete.Name = "cmdDelete";
             this.cmdDelete.Size = new System.Drawing.Size(149, 30);
             this.cmdDelete.TabIndex = 12;
             this.cmdDelete.Text = "Delete";
             this.cmdDelete.UseVisualStyleBackColor = true;
+            this.cmdDelete.Click += new System.EventHandler(this.cmdDelete_Click);
             // 
-            // dataGridView1
+            // dg1
             // 
-            this.dataGridView1.AllowUserToAddRows = false;
-            this.dataGridView1.AllowUserToDeleteRows = false;
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(218, 31);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.ReadOnly = true;
-            this.dataGridView1.Size = new System.Drawing.Size(402, 369);
-            this.dataGridView1.TabIndex = 13;
+            this.dg1.AllowUserToAddRows = false;
+            this.dg1.AllowUserToDeleteRows = false;
+            this.dg1.AllowUserToResizeColumns = false;
+            this.dg1.AllowUserToResizeRows = false;
+            this.dg1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dg1.Location = new System.Drawing.Point(218, 31);
+            this.dg1.Name = "dg1";
+            this.dg1.ReadOnly = true;
+            this.dg1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dg1.Size = new System.Drawing.Size(665, 404);
+            this.dg1.TabIndex = 13;
+            // 
+            // txtTableNum
+            // 
+            this.txtTableNum.Location = new System.Drawing.Point(31, 145);
+            this.txtTableNum.Name = "txtTableNum";
+            this.txtTableNum.Size = new System.Drawing.Size(40, 20);
+            this.txtTableNum.TabIndex = 14;
+            // 
+            // txtTime
+            // 
+            this.txtTime.Location = new System.Drawing.Point(31, 91);
+            this.txtTime.Name = "txtTime";
+            this.txtTime.Size = new System.Drawing.Size(100, 20);
+            this.txtTime.TabIndex = 15;
+            // 
+            // txtDate
+            // 
+            this.txtDate.Location = new System.Drawing.Point(31, 38);
+            this.txtDate.Name = "txtDate";
+            this.txtDate.Size = new System.Drawing.Size(100, 20);
+            this.txtDate.TabIndex = 16;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 175);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Duration";
+            // 
+            // txtDuration
+            // 
+            this.txtDuration.Location = new System.Drawing.Point(31, 191);
+            this.txtDuration.Name = "txtDuration";
+            this.txtDuration.Size = new System.Drawing.Size(38, 20);
+            this.txtDuration.TabIndex = 18;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(635, 411);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(901, 453);
+            this.Controls.Add(this.txtDuration);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtDate);
+            this.Controls.Add(this.txtTime);
+            this.Controls.Add(this.txtTableNum);
+            this.Controls.Add(this.dg1);
             this.Controls.Add(this.cmdDelete);
             this.Controls.Add(this.cmdUpdate);
             this.Controls.Add(this.cmdInsert);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.comboBox3);
-            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.txtTel);
+            this.Controls.Add(this.txtName);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "RESO";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dg1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -200,16 +225,18 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.ComboBox comboBox2;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtTel;
         private System.Windows.Forms.Button cmdInsert;
         private System.Windows.Forms.Button cmdUpdate;
         private System.Windows.Forms.Button cmdDelete;
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dg1;
+        private System.Windows.Forms.TextBox txtTableNum;
+        private System.Windows.Forms.TextBox txtTime;
+        private System.Windows.Forms.TextBox txtDate;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtDuration;
     }
 }
 
--- HotelReso/Form1.cs
+++ HotelReso/Form1.cs
@@ -7,28 +7,201 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace HotelReso
 {
     public partial class Form1 : Form
-    {//test
-        //test
-        //test
+    {
+        //globals
+        private SqlConnection conn = null;
+        private SqlDataAdapter da = null;
+        private DataSet ds = null;
+
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label3_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getData();
+        }
+        //private void label3_Click(object sender, EventArgs e)
+        //{
+
+        //}
+
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    MessageBox.Show("I can't believe you actually pressed it!", "I'm Impressed!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+        //}
+
+        private void getData()
+        {
+            string connStr = "Data Source=(LocalDB)\\v11.0;AttachDbFilename=C:\\PROG37721\\HotelReso\\HotelReso\\Reservations.mdf;Integrated Security=True";
+            try
+            {
+                conn = new SqlConnection(connStr);
+                conn.Open();
+                string sql = "SELECT * FROM [tReservations]";
+                da = new SqlDataAdapter(sql, conn);
+                SqlCommandBuilder sb = new SqlCommandBuilder(da);
+                ds = new DataSet();
+                da.Fill(ds, "tReservations");
+
+                DataView myView = new DataView(ds.Tables["tReservations"]);
+                dg1.DataSource = myView;
+                dg1.ClearSelection();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error Connecting to Database");
+            }
+            finally
+            {
+                if (conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+
+        private void cmdInsert_Click(object sender, EventArgs e)
+        {
+            if(dataGood())
+            {
+                if(isValidReservation("i"))
+                {
+                    DataRow dr = ds.Tables["tReservations"].NewRow();
+                    dr["TableNo"] = Convert.ToInt32(txtTableNum.Text);
+                    dr["Date"] = txtDate.Text;
+                    dr["Time"] = txtTime.Text;
+                    dr["Duration"] = Convert.ToDouble(txtDuration.Text);
+                    dr["Name"] = txtName.Text;
+                    dr["Telephone"] = txtTel.Text;
+
+                    ds.Tables["tReservations"].Rows.Add(dr);
+                    updateDB();
+                    clearText();
+                }
+            }
+        }
+
+        private void cmdUpdate_Click(object sender, EventArgs e)
         {
 
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void cmdDelete_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("I can't believe you actually pressed it!", "I'm Impressed!", MessageBoxButtons.OK, MessageBoxIcon.Warning, MessageBoxDefaultButton.Button1);
+
         }
 
+        private void updateDB()
+        {
+            try
+            {
+                conn.Open();
+                da.Update(ds, "tReservations");
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message, "Error Updating Database");
+
+            }
+            finally
+            {
+                if(conn != null)
+                {
+                    conn.Close();
+                }
+            }
+        }
+
+        private string isLeapYear(string year)
+        {
+            string whichYear = null;
+
+            if(Convert.ToInt32(year) % 4 != 0)
+            {
+                whichYear = "c";
+            }
+            else if(Convert.ToInt32(year) % 100 != 0 )
+            {
+                whichYear = "l";
+            }
+            else if(Convert.ToInt32(year) % 400 != 0)
+            {
+                whichYear = "c";
+            }
+            else
+            {
+                whichYear = "l";
+            }
+            return whichYear;
+        }
+
+        private void clearText()
+        {
+            txtDate.Text = "";
+            txtTime.Text = "";
+            txtTableNum.Text = "";
+            txtDuration.Text = "";
+            txtName.Text = "";
+            txtTel.Text = "";
+            txtDate.Focus();
+            dg1.ClearSelection();
+        }
+
+        private bool dataGood()
+        {
+            return true;
+        }
+
+        private bool isValidReservation(string state)
+        {
+            if(state.Equals("i"))
+            {
+                for (int i = 0; i < dg1.Rows.Count; i++)
+                {
+                    if(txtDate.Text.Equals(dg1.Rows[i].Cells[0].Value.ToString()))
+                    {
+                        if(txtTableNum.Text.Equals(dg1.Rows[i].Cells[2].Value.ToString()))
+                        {
+                            if(txtTime.Text.Equals(dg1.Rows[i].Cells[1].Value.ToString()))
+                            {
+                                MessageBox.Show("A reservation already exists for this table at this time. Please select a different time or a different table", "Invalid Reservation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                txtTableNum.Focus();
+                                return false;
+                            }
+                            //else if(txtTime)
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+
+        private void setControlState(string state)
+        {
+            if (state.Equals("i"))
+            {
+                cmdInsert.Enabled = true;
+                cmdUpdate.Enabled = false;
+                cmdDelete.Enabled = false;
+                clearText();
+            }
+            if (state.Equals("u/d"))
+            {
+                cmdInsert.Enabled = false;
+                cmdUpdate.Enabled = true;
+                cmdDelete.Enabled = true;
+            }
+        }
+
+        
         //add form load
     }
 }
